917a0af2c3670ea74f0596e10e6918ed7c0b7e09
EricZhao666 <78531180+EricZhao666@users.noreply.github.com>
2021-02-08 20:24:58 +0800

    Add files via upload

---
+++ shujuku.sql
@@ -0,0 +1,6 @@
+INSERT INTO Student VALUES(1,'tom','boy','peter',1);
+INSERT INTO Student VALUES(2,'eric','boy','peter',1);
+INSERT INTO Student VALUES(3,'qwe','boy','wang',2);
+INSERT INTO Student VALUES(4,'siri','girl','wang',2);
+INSERT INTO teacher VALUES('wang');
+INSERT INTO teacher VALUES('peter');
